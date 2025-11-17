87e1b628a867d58c4dc7b97e570274abef8252ab
Gil-Lael <ac.kouakou@ecole-ipssi.net>
2025-11-17 15:13:04 +0100

    deuxieme tp

---
+++ tp2/tp2.php
@@ -0,0 +1,13 @@
+<?php
+function my_strrev(string $s): string {
+    $len = strlen($s);
+    $rev = '';
+
+    for ($i = $len - 1; $i >= 0; $i--) {
+        $rev .= $s[$i];
+    }
+
+    return $rev;
+}
+echo my_strrev("Coucou");
+?>
